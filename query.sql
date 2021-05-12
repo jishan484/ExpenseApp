da6e115c612c02fcc7bd3325fae57e84ce8d9c75
jishan <49402826+jishan484@users.noreply.github.com>
2021-05-12 12:17:22 +0530

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,11 @@
+create database testdb;
+use testdb;
+
+CREATE TABLE `expense` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item` varchar(45) NOT NULL,
+  `amount` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+select * from expense;
